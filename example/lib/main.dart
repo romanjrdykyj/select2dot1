3295a8d32155d24c24e79138ab843cd09d548a42
ROMAN <romanjrdykyj@gmail.com>
2022-12-18 11:59:50 +0100

    add loader, change name app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'select2dot1_example',
       initialRoute: Menu.routeName,
       routes: <String, WidgetBuilder>{
         Menu.routeName: (context) => const Menu(),
